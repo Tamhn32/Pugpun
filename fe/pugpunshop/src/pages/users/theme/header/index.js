319fdefc3bc061a18382d60b15e9c6a8cacacfb1
Tam Nguyen <hoangtam32@gmail.com>
2024-01-25 00:02:23 -0600

    done header

--- fe/pugpunshop/src/pages/users/theme/header/index.js
+++ fe/pugpunshop/src/pages/users/theme/header/index.js
@@ -1,7 +1,55 @@
 import { memo } from "react";
+import "./style.scss";
+import {
+  BiLogoFacebookSquare,
+  BiLogoInstagramAlt,
+  BiLogoTwitter,
+  BiLogoLinkedinSquare,
+  BiSolidUser,
+} from "react-icons/bi";
+
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div className="container">Header</div>;
+  return (
+    <div className="header_top">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">Trai</div>
+          <div className="col-6 header_top_right">
+            <ul>
+              <li>
+                <Link to={""}>
+                  <BiLogoFacebookSquare />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <BiLogoInstagramAlt />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <BiLogoTwitter />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <BiLogoLinkedinSquare />
+                </Link>
+              </li>
+              <li>
+                <Link to={""}>
+                  <BiSolidUser />
+                </Link>
+                <span> Sign In</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default memo(Header);
