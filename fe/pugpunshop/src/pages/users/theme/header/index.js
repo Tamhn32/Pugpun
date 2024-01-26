1a3b15450ec848d321577450229144cda73c2beb
Tam Nguyen <hoangtam32@gmail.com>
2024-01-25 22:48:22 -0600

    UI desgin

--- fe/pugpunshop/src/pages/users/theme/header/index.js
+++ fe/pugpunshop/src/pages/users/theme/header/index.js
@@ -6,49 +6,68 @@ import {
   BiLogoTwitter,
   BiLogoLinkedinSquare,
   BiSolidUser,
+  BiLogoGmail,
 } from "react-icons/bi";
+import { formatter } from "utils/fomater";
 
 import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="header_top">
-      <div className="container">
-        <div className="row">
-          <div className="col-6">Trai</div>
-          <div className="col-6 header_top_right">
-            <ul>
-              <li>
-                <Link to={""}>
-                  <BiLogoFacebookSquare />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <BiLogoInstagramAlt />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <BiLogoTwitter />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <BiLogoLinkedinSquare />
-                </Link>
-              </li>
-              <li>
-                <Link to={""}>
-                  <BiSolidUser />
-                </Link>
-                <span> Sign In</span>
-              </li>
-            </ul>
+    <>
+      <div className="header_top">
+        <div className="container">
+          <div className="row">
+            <div className="col-6 header_top_left">
+              <ul>
+                <li>
+                  <BiLogoGmail />
+                  hoangtam@gmail.com
+                </li>
+                <li>freeshipping from {formatter(100)} </li>
+              </ul>
+            </div>
+            <div className="col-6 header_top_right">
+              <ul>
+                <li>
+                  <Link to={""}>
+                    <BiLogoFacebookSquare />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <BiLogoInstagramAlt />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <BiLogoTwitter />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <BiLogoLinkedinSquare />
+                  </Link>
+                </li>
+                <li>
+                  <Link to={""}>
+                    <BiSolidUser />
+                  </Link>
+                  <span> Sign In</span>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-3">Logo</div>
+          <div className="col-xl-6 ">Menu</div>
+          <div className="col-xl-3 ">Phone</div>
+        </div>
+      </div>
+    </>
   );
 };
 
